7ea60df6b927065d4b73acb16f8f6b5a5ca265f2
kilhwan <eggkimkr@gmail.com>
2022-10-05 19:25:52 +0900

    use material

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import { Component } from 'react';
 import './App.css';
 import Customer from './componets/Customer';
+import { Table, TableRow } from '@material-ui/core';
+import {TableBody} from '@material-ui/core';
+import {TableCell} from '@material-ui/core';
+import {withStyles} from '@material-ui/core';
+import {Paper} from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit *3,
+    overflowX: "auto"
+  },
+  table:{
+    minWidth:1080,
+
+  }
+
+})
+
 const customers = [{
   'id':1,
   'image':'https://placeimg.com/64/64/any',
@@ -13,19 +32,19 @@ const customers = [{
 {
   'id':2,
   'image':'https://placeimg.com/64/64/any2',
-  'name': '김길환2',
+  'name': '황주영',
   'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
+  'gender': '여자',
+  'job': '리더'
 
 },
 {
   'id':3,
   'image':'https://placeimg.com/64/64/any3',
-  'name': '김길환3',
+  'name': '이대규',
   'birthday': '961222',
   'gender': '남자',
-  'job': '대학생'
+  'job': '센터장'
 
 }
 ]
@@ -33,20 +52,32 @@ const customers = [{
 class App extends Component {
   render(){
   return (
-    <div>
+    <Paper className={this.props.root}>
+    <Table className={this.props.table}>
+      <TableRow>
+        <TableCell>번호</TableCell>
+        <TableCell>이미지</TableCell>
+        <TableCell>이름</TableCell>
+        <TableCell>생년월일</TableCell>
+        <TableCell>성별</TableCell>
+        <TableCell>직업</TableCell>
+      </TableRow>
+
+    <TableBody>
     {
       customers.map(c=> {
-        return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} genger={c.gender} job={c.job} />
+        return (
+                <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job} />
         )
-
-
       })
 
     }
-   </div>
+    </TableBody>
+    </Table>
+   </Paper>
     
   );  
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
